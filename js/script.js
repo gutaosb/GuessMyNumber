505627d1f5232984601baa6b036bea3e85c391a1
gutaosb <augustosemensato44@gmail.com>
2025-03-11 21:54:37 -0300

    Improving logic

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 'use strict'
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
-document.querySelector('.number').textContent = secretNumber
+let secretNumber = Math.trunc(Math.random() * 20) + 1
+// document.querySelector('.number').textContent = secretNumber
 
 let score = 20
 
-
+// check button
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value)
 
+    // when there is no input
     if(!guess){
         document.querySelector('.message').textContent = '⛔ No number!'
     
+    //when player wins
     }else if(guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct number!'
     
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem'
+        document.querySelector('.number').textContent = secretNumber
+
+
+    // when gess is too high
     }else if(guess > secretNumber){
 
         if(score > 1){
@@ -25,9 +34,8 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = '😓 You lost the game'
             document.querySelector('.score').textContent = 0
         }
-
-        
-
+    
+    // when gess is too low
     }else if(guess < secretNumber){
 
         if(score > 1){
@@ -42,4 +50,17 @@ document.querySelector('.check').addEventListener('click', function() {
     }
 
 
+})
+
+// again button
+document.querySelector('.again').addEventListener('click', function () {
+    secretNumber = Math.trunc(Math.random() * 20) + 1
+    score = 20
+
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').textContent = ''
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
 })
